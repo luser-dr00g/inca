fc1fe17e717841da0385b0b03b5ae8fb1c363474
luser-droog <luser.droog@gmail.com>
2014-03-22 17:27:18 -0500

    define identity elements for monadic use of dyadic cat,minus,times

--- inca.c
+++ inca.c
@@ -23,20 +23,20 @@ I tr(I r,I*d){I z=1;DO(r,z=z*d[i]);R z;} //table rank
 A ga(I t,I r,I*d){A z=(A)ma(5+tr(r,d));z->t=t,z->r=r,mv(z->d,d,r);R z;}
 
 V1(iota){I n=*w->p;A z=ga(0,1,&n);DO(n,z->p[i]=i);R z;}
-V2(plus){I r=w->r,*d=w->d,n=tr(r,d);A z=ga(0,r,d); \
+V2(plus){I r=w->r,*d=w->d,n=tr(r,d);A z=ga(0,r,d);
     DO(n,z->p[i]=a->p[i]+w->p[i]);R z;}
-V2(minus){I r=w->r,*d=w->d,n=tr(r,d);A z=ga(0,r,d); \
+V2(minus){I r=w->r,*d=w->d,n=tr(r,d);A z=ga(0,r,d);
     DO(n,z->p[i]=a->p[i]-w->p[i]);R z;}
-V2(times){I r=w->r,*d=w->d,n=tr(r,d);A z=ga(0,r,d); \
+V2(times){I r=w->r,*d=w->d,n=tr(r,d);A z=ga(0,r,d);
     DO(n,z->p[i]=a->p[i]*w->p[i]);R z;}
-V2(from){I r=w->r-1,*d=w->d+1,n=tr(r,d); \
+V2(from){I r=w->r-1,*d=w->d+1,n=tr(r,d);
     A z=ga(w->t,r,d);mv(z->p,w->p+(n**a->p),n);R z;}
 V1(box){A z=ga(1,0,0);*z->p=(I)w;R z;}
 V1(unbox){R *w->p;}
-V2(cat){I an=tr(a->r,a->d),wn=tr(w->r,w->d),n=an+wn; \
+V2(cat){I an=tr(a->r,a->d),wn=tr(w->r,w->d),n=an+wn;
     A z=ga(w->t,1,&n);mv(z->p,a->p,an);mv(z->p+an,w->p,wn);R z;}
 V2(find){}
-V2(reshape){I r=a->r?*a->d:1,n=tr(r,a->p),wn=tr(w->r,w->d); \
+V2(reshape){I r=a->r?*a->d:1,n=tr(r,a->p),wn=tr(w->r,w->d);
     A z=ga(w->t,r,a->p);mv(z->p,w->p,wn=n>wn?wn:n); //wn=min(wn,n) 
     if(n-=wn)mv(z->p+wn,z->p,n);R z;}
 V1(shape){A z=ga(0,1,&w->r);mv(z->p,w->d,w->r);R z;}
@@ -58,7 +58,13 @@ qv(unsigned a){R a<'a';}
 
 A ex(I*e){I a=*e,w=e[1];
     if(qp(a)&&w=='=')R st[a-'a']=ex(e+2);
-    if (qv(a)) R (*vm[a])(ex(e+1));
+    if (qv(a)){I m=a;
+        if (vm[m]==0){
+            a=vid[m];
+            R (*vd[m])(a,ex(e+1));
+        }
+        R (*vm[m])(ex(e+1));
+    }
     if (w&&qv(w)){I d=w;
         w=ex(e+2);
         if (qp(a)) a=st[a-'a'];
@@ -73,4 +79,6 @@ verb(c){I i=0;for(;vt[i];)if(vt[i++]==c)R i;R 0;}
 I*wd(C*s){I a,n=strlen(s),*e=ma(n+1);C c;
     DO(n,e[i]=(a=noun(c=s[i]))?a:(a=verb(c))?a:c);e[n]=0;R e;}
 
-main(){C s[99];while(gets(s))pr(ex(wd(s)));}
+main(){C s[99];
+    vid[6]=vid[8]=noun('0');vid[9]=noun('1');
+    while(gets(s))pr(ex(wd(s)));}
