21fa4b7134324ee728a0979fa6ec0449650c8998
luser-droog <luser.droog@gmail.com>
2014-03-25 09:57:10 -0500

    replace '=' for assignment with '<'.

--- inca.c
+++ inca.c
@@ -21,8 +21,9 @@ I ma(I n){R malloc(n*sizeof(I));}
 mv(I*d,I*s,I n){DO(n,d[i]=s[i]);}
 I tr(I r,I*d){I z=1;DO(r,z=z*d[i]);R z;} //table rank
 A ga(I t,I r,I*d){A z=(A)ma(5+tr(r,d));z->t=t,z->r=r,mv(z->d,d,r);R z;}
-A cp(A w){
-    A z=ga(w->t,w->r,w->d);if(w->r)mv(z->p,w->p,tr(w->r,w->d));else *z->p=*w->p;
+A cp(A w){I n=tr(w->r,w->d);
+    A z=ma(5+ (n?n:1));z->t=w->t;z->r=0;*z->p=*w->p;
+    DO(n,z->p[i]=w->p[i]);
     R z;
 }
 
@@ -80,8 +81,8 @@ V1(shape){A z=ga(0,1,&w->r);mv(z->p,w->d,w->r);R z;}
 V1(identity){R w;}
 V1(size){A z=ga(0,1,&w->r);mv(z->p,w->d,w->r);R z;}
 
-A reduce(A w,I f){
-}
+A reduce(A w,I f);
+A dot(A a,I f,I g,A w);
 
 pi(i){P("%d ",i);}
 nl(){P("\n");}
@@ -91,24 +92,32 @@ pr(A w){I r=w->r,*d=w->d,n=tr(r,d); DO(r,pi(d[i]));nl();
 I st[26];
 qp(a){R a>='a'&&a<='z';}
 
-enum   {      PLUS=1,   FROM, IOTA, BOX, SHAPE,   CAT, UNBOX, MINUS, TIMES, DIVIDE, BAR,      AND, OR,  NOT, NV  };
-C vt[]={      '+',      '{',  '~', '<', '#',      ',', '>',    '-',   '*',  '%',    '|',     '&',  '^', '`', 0 };
-A(*vd[])()={0,plus,     from, find, 0,   reshape, cat, 0,     minus, times, divide, modulus,  and, or,  0},
- (*vm[])()={0,identity, size, iota, box, shape,   0,   unbox, 0,     0,     0,      absolute, 0,   0,   not};
-I vid[]={0,   0,        0,    0,    0,   0,       0,   0,     0,     1,     1,      2,        1,   0,   0};
-qv(unsigned a){R a<'a'&&a<NV;}
+enum   {      PLUS=1,   FROM, IOTA, BOX, SHAPE,   CAT, UNBOX, MINUS, TIMES, DIVIDE, BAR,      AND, OR,  NOT, SLASH, DOT, NV  };
+C vt[]={      '+',      '{',  '~', '<', '#',      ',', '>',    '-',   '*',  '%',    '|',     '&',  '^', '`', '/',   '.', 0 };
+A(*vd[])()={0,plus,     from, find, 0,   reshape, cat, 0,     minus, times, divide, modulus,  and, or,  0,    0,    0,   0},
+ (*vm[])()={0,identity, size, iota, box, shape,   0,   unbox, 0,     0,     0,      absolute, 0,   0,   not,  0,    0,   0};
+A(*od[])()={0,0,        0,    0,    0,   0,       0,   0,     0,     0,     0,      0,        0,   0,   0,    0,    dot,   0},
+ (*om[])()={0,0,        0,    0,    0,   0,       0,   0,     0,     0,     0,      0,        0,   0,   0,    reduce, 0,   0};
+I vid[]={0,   0,        0,    0,    0,   0,       0,   0,     0,     1,     1,      2,        1,   0,   0,    0,    0,   0};
+qv(unsigned a){R a<'a'&&a<NV&&(vd[a]||vm[a]);}
+qo(unsigned a){R a<'a'&&a<NV&&(od[a]||om[a]);}
 
-enum   {       SLASH=1, DOT, NO  };
-C ot[]={       '/',     '.', 0};
-A(*od[])()={ 0, 0,      dot },
- (*om[])()={ 0, reduce, 0 };
-qo(unsigned a){R a<'a'&&a>NV&&a<NV+NO;}
+A reduce(A w,I f){
+    I r=w->r,*d=w->d,n=tr(r,d);
+    printf("reduce: %u %u\n",w,f);
+    A z=cp(vid[f]);
+    DO(n,z=(*vd[f])(z,w->p[i]));
+    R z;
+}
+A dot(A a,I f,I g,A w){
+}
 
-A ex(I*e){I a=*e,w=e[1];
+A ex(I*e){I a=*e,w=e[1]; I d=w;
 EX:
-    if(qp(a)&&w=='=')R st[a-'a']=ex(e+2);
+    if(qp(a)&&w==BOX)R st[a-'a']=ex(e+2);
     if (qv(a)){I m=a;
         if (qo(w)){
+            R (*om[w])(ex(e+2),a);
         }
         if (vm[m]==0){
             a=vid[m];
@@ -116,32 +125,10 @@ EX:
         }
         R (*vm[m])(ex(e+1));
     }
-    if (w&&qv(w)){I d=w;
-        if (w==' '){ //e:"a bc..." A=cat(a,b)
-            A _d,_w;
-            w=e[2];
-            if (qp(a)) a=st[a-'a'];
-            if (qp(w)) w=st[w-'a'];
-            e+=2;                  //e:"Ac..."
-            d=e[1];
-            while (!qv(d)){
-                if (qp(d)) d=st[d-'a'];
-                _d=d;
-                if (_d->r==0){
-                    _w=w;
-                    *_d->p+=*_w->p*10;
-                    w=d;
-                    ++e;
-                    d=e[1];
-                    continue;
-                }
-                break;
-            }
-            a=cat((A)a,(A)w);
-            w=e[1];
-            goto EX;
-        }
+    if (w&&qv(w)){
+
         if (qo(e[2])){
+            R (*od[e[2]])(a,w,e[3],ex(e+4));
         }
         w=ex(e+2);
         if (qp(a)) a=st[a-'a'];
@@ -149,22 +136,48 @@ EX:
     }
     if (qp(a)) a=st[a-'a'];
     if (w){ //!qv
-        A _a=a, _w=w;
-        if (qp(w)) w=st[w-'a'];
-        if (_a->r==0 && _w->r==0){
-            *_w->p+=*_a->p*10;
-            a=w;
-            ++e;
-            w=e[1];
-            goto EX;
+
+        if (w==' '){ //e:"a bc..." A=cat(a,b)
+            A _d,_w;
+            w=e[2];
+            if (w){
+                if (qp(w)) w=cp(st[w-'a']);
+                e+=2;                  //e:"Ac..."
+                d=e[1];
+                while (d&&!qv(d)&&d!=' '){
+                    if (qp(d)) cp(d=st[d-'a']);
+                    _d=d;
+                    if (_d->r==0){
+                        _w=w;
+                        *_d->p+=*_w->p*10;
+                        w=d;
+                        ++e;
+                        d=e[1];
+                        continue;
+                    }
+                    break;
+                }
+                a=cat((A)a,(A)w);
+                w=e[1];
+                goto EX;
+            }
+        } else {
+            A _a=a, _w=w;
+            if (qp(w)) cp(w=st[w-'a']);
+            if (_a->r==0 && _w->r==0){
+                *_w->p+=*_a->p*10;
+                a=w;
+                ++e;
+                w=e[1];
+                goto EX;
+            }
         }
     }
     R (A)a; 
 }
 
 noun(c){A z;if(c<'0'||c>'9')R 0;z=ga(0,0,0);*z->p=c-'0';R z;}
-verb(c){I i=0;for(;vt[i];)if(vt[i++]==c)R i;
-    for(i=0;ot[i];)if(ot[i++]==c)R i+NV;R 0;}
+verb(c){I i=0;for(;vt[i];)if(vt[i++]==c)R i;R 0;}
 I*wd(C*s){I a,n=strlen(s),*e=ma(n+1);C c;
     DO(n,e[i]=(a=noun(c=s[i]))?a:(a=verb(c))?a:c);
     e[n]=0;R e;}
