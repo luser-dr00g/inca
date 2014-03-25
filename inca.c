a53f3702bd58268c30c9154d8252d1b3999d04cb
luser-droog <luser.droog@gmail.com>
2014-03-25 10:48:54 -0500

    fix reduce: +/~9 => 36  +/~10 =>45

--- inca.c
+++ inca.c
@@ -17,12 +17,12 @@ typedef struct a{I t,r,d[3],p[2];} *A;
 #define V2(f) A f(A a,A w)
 #define DO(n,x) {I i=0,_n=(n);for(;i<_n;++i){x;}}
 
-I ma(I n){R malloc(n*sizeof(I));}
+I ma(I n){R (I)malloc(n*sizeof(I));}
 mv(I*d,I*s,I n){DO(n,d[i]=s[i]);}
 I tr(I r,I*d){I z=1;DO(r,z=z*d[i]);R z;} //table rank
 A ga(I t,I r,I*d){A z=(A)ma(5+tr(r,d));z->t=t,z->r=r,mv(z->d,d,r);R z;}
 A cp(A w){I n=tr(w->r,w->d);
-    A z=ma(5+ (n?n:1));z->t=w->t;z->r=0;*z->p=*w->p;
+    A z=(A)ma(5+ (n?n:1));z->t=w->t;z->r=0;*z->p=*w->p;
     DO(n,z->p[i]=w->p[i]);
     R z;
 }
@@ -39,7 +39,9 @@ A cp(A w){I n=tr(w->r,w->d);
     R z;
 
 V1(iota){I n=*w->p;A z=ga(0,1,&n);DO(n,z->p[i]=i);R z;}
-V2(plus){I r=w->r,*d=w->d,n=tr(r,d);A z=ga(0,r,d);
+V2(plus){
+    //printf("plus %d %d\n", *a->p, *w->p);
+    I r=w->r,*d=w->d,n=tr(r,d);A z=ga(0,r,d);
     OP(+)
 }
 V2(minus){I r=w->r,*d=w->d,n=tr(r,d);A z=ga(0,r,d);
@@ -70,7 +72,7 @@ V1(not){I r=w->r,*d=w->d,n=tr(r,d);A z=ga(0,r,d);
 V2(from){I r=w->r-1,*d=w->d+1,n=tr(r,d);
     A z=ga(w->t,r,d);mv(z->p,w->p+(n**a->p),n);R z;}
 V1(box){A z=ga(1,0,0);*z->p=(I)w;R z;}
-V1(unbox){R *w->p;}
+V1(unbox){R (A)*w->p;}
 V2(cat){I an=tr(a->r,a->d),wn=tr(w->r,w->d),n=an+wn;
     A z=ga(w->t,1,&n);mv(z->p,a->p,an);mv(z->p+an,w->p,wn);R z;}
 V2(find){}
@@ -92,21 +94,27 @@ pr(A w){I r=w->r,*d=w->d,n=tr(r,d); DO(r,pi(d[i]));nl();
 I st[26];
 qp(a){R a>='a'&&a<='z';}
 
-enum   {      PLUS=1,   FROM, IOTA, BOX, SHAPE,   CAT, UNBOX, MINUS, TIMES, DIVIDE, BAR,      AND, OR,  NOT, SLASH, DOT, NV  };
-C vt[]={      '+',      '{',  '~', '<', '#',      ',', '>',    '-',   '*',  '%',    '|',     '&',  '^', '`', '/',   '.', 0 };
-A(*vd[])()={0,plus,     from, find, 0,   reshape, cat, 0,     minus, times, divide, modulus,  and, or,  0,    0,    0,   0},
- (*vm[])()={0,identity, size, iota, box, shape,   0,   unbox, 0,     0,     0,      absolute, 0,   0,   not,  0,    0,   0};
-A(*od[])()={0,0,        0,    0,    0,   0,       0,   0,     0,     0,     0,      0,        0,   0,   0,    0,    dot,   0},
- (*om[])()={0,0,        0,    0,    0,   0,       0,   0,     0,     0,     0,      0,        0,   0,   0,    reduce, 0,   0};
-I vid[]={0,   0,        0,    0,    0,   0,       0,   0,     0,     1,     1,      2,        1,   0,   0,    0,    0,   0};
+enum   {         PLUS=1, FROM, IOTA, BOX, SHAPE,   CAT, UNBOX, MINUS, TIMES, DIVIDE, BAR,      AND, OR,  NOT, SLASH,   DOT, NV  };
+C vt[]={         '+',    '{',  '~', '<', '#',      ',', '>',    '-',   '*',  '%',    '|',     '&',  '^', '`', '/',     '.', 0 };
+A(*vd[])(A,A)={0,plus,   from, find, 0,   reshape, cat, 0,     minus, times, divide, modulus,  and, or,  0,    0,      0,   0},
+ (*vm[])(A)={0,identity, size, iota, box, shape,   0,   unbox, 0,     0,     0,      absolute, 0,   0,   not,  0,      0,   0};
+A(*od[])(A,I,I,A)={0,0,  0,    0,    0,   0,       0,   0,     0,     0,     0,      0,        0,   0,   0,    0,      dot, 0},
+ (*om[])(A,I)={0, 0,     0,    0,    0,   0,       0,   0,     0,     0,     0,      0,        0,   0,   0,    reduce, 0,   0};
+I vid[]={0,       '0',   0,    0,    0,   0,       0,   0,     0,     '1',   1,      2,        '1', '0',  0,    0,      0,   0};
 qv(unsigned a){R a<'a'&&a<NV&&(vd[a]||vm[a]);}
 qo(unsigned a){R a<'a'&&a<NV&&(od[a]||om[a]);}
 
 A reduce(A w,I f){
     I r=w->r,*d=w->d,n=tr(r,d);
-    printf("reduce: %u %u\n",w,f);
-    A z=cp(vid[f]);
-    DO(n,z=(*vd[f])(z,w->p[i]));
+    //printf("reduce: %u %u\n",w,f); pr(w);
+    A z=(A)noun(vid[f]);
+    //printf("n = %d, z = %d, *w->p = %d\n", n, *z->p, *w->p); fflush(0);
+    if (w->r){
+        A ind = noun('0');;
+        DO(n,*ind->p=i;z=(*vd[f])(z,from(ind,w));/*printf("z = %d\n", *z->p);*/);
+    } else {
+        z=(*vd[f])(z,w);
+    }
     R z;
 }
 A dot(A a,I f,I g,A w){
@@ -120,22 +128,22 @@ EX:
             R (*om[w])(ex(e+2),a);
         }
         if (vm[m]==0){
-            a=vid[m];
+            a=noun(vid[m]);
             R (*vd[m])(a,ex(e+1));
         }
         R (*vm[m])(ex(e+1));
     }
-    if (w&&qv(w)){
+    if (w){
+        if (qv(w)){
 
-        if (qo(e[2])){
-            R (*od[e[2]])(a,w,e[3],ex(e+4));
+            if (qo(e[2])){
+                R (*od[e[2]])(a,w,e[3],ex(e+4));
+            }
+            w=ex(e+2);
+            if (qp(a)) a=st[a-'a'];
+            R (*vd[d])(a,w);
         }
-        w=ex(e+2);
         if (qp(a)) a=st[a-'a'];
-        R (*vd[d])(a,w);
-    }
-    if (qp(a)) a=st[a-'a'];
-    if (w){ //!qv
 
         if (w==' '){ //e:"a bc..." A=cat(a,b)
             A _d,_w;
@@ -158,7 +166,7 @@ EX:
                     break;
                 }
                 a=cat((A)a,(A)w);
-                w=e[1];
+                d=w=e[1];
                 goto EX;
             }
         } else {
@@ -168,7 +176,7 @@ EX:
                 *_w->p+=*_a->p*10;
                 a=w;
                 ++e;
-                w=e[1];
+                d=w=e[1];
                 goto EX;
             }
         }
@@ -183,5 +191,5 @@ I*wd(C*s){I a,n=strlen(s),*e=ma(n+1);C c;
     e[n]=0;R e;}
 
 main(){C s[99];
-    vid[6]=vid[8]=noun('0');vid[9]=vid[10]=noun('1');vid[11]=noun('2');vid[12]=noun('1');vid[13]=noun('0');
+    //vid[1]=vid[6]=vid[8]=noun('0');vid[9]=vid[10]=noun('1');vid[11]=noun('2');vid[12]=noun('1');vid[13]=noun('0');
     while(gets(s))pr(ex(wd(s)));}
