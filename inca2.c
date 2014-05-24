efd6198c39cbb204d56910849fef0b3852f40041
luser-droog <luser.droog@gmail.com>
2014-05-24 02:22:49 -0500

    break statements into lines. replace char tests with ctype.h functions.

--- inca2.c
+++ inca2.c
@@ -22,11 +22,12 @@ typedef struct a{I k,t,n,r,d[3],p[1];}*ARC;
 #define V2(f) ARC f(ARC a,ARC w)
 #define DO(n,x) {I i=0,_n=(n);for(;i<_n;++i){x;}}
 
-I *ma(n){R(I*)malloc(n*sizeof(intptr_t));}
+I *ma(n){R(I*)malloc(n*sizeof(I));}
 mv(d,s,n)I *d,*s;{DO(n,d[i]=s[i]);}
 tr(r,d)I *d;{I z=1;DO(r,z=z*d[i]);R z;}
 ARC ga(t,r,d)I *d;{I n;
-    ARC z=(ARC)ma((sizeof(*z)/sizeof(I))+(n=tr(r,d)));AT(z)=t,AR(z)=r,AN(z)=n,mv(AD(z),d,r);
+    ARC z=(ARC)ma((sizeof(*z)/sizeof(I))+(n=tr(r,d)));
+    AT(z)=t,AR(z)=r,AN(z)=n,mv(AD(z),d,r);
     R z;}
 V1(iota){
     I n=*AV(w);ARC z=ga(INT,1,&n);DO(n,AV(z)[i]=i);R z;}
@@ -40,14 +41,17 @@ V1(box){
     ARC z=ga(BOX,0,0);*AV(z)=(I)w;R z;}
 V2(cat){
     I an=AN(a),wn=AN(w),n=an+wn;
-    ARC z=ga(AT(w),1,&n);mv(AV(z),AV(a),an);mv(AV(z)+an,AV(w),wn);R z;}
+    ARC z=ga(AT(w),1,&n);
+    mv(AV(z),AV(a),an);mv(AV(z)+an,AV(w),wn);R z;}
 V2(find){}
 V2(rsh){
     I r=AR(a)?*AD(a):1,n=tr(r,AV(a)),wn=AN(w);
-    ARC z=ga(AT(w),r,AV(a));mv(AV(z),AV(w),wn=n>wn?wn:n);
+    ARC z=ga(AT(w),r,AV(a));
+    mv(AV(z),AV(w),wn=n>wn?wn:n);
     if(n-=wn)mv(AV(z)+wn,AV(z),n);R z;}
 V1(sha){
-    ARC z=ga(INT,1,&AR(w));mv(AV(z),AD(w),AR(w));R z;}
+    ARC z=ga(INT,1,&AR(w));
+    mv(AV(z),AD(w),AR(w));R z;}
 V1(id){R w;}
 V1(size){
     ARC z=ga(INT,0,0);*AV(z)=AR(w)?*AD(w):1;R z;}
@@ -55,9 +59,12 @@ V1(size){
 pi(i){printf("%d ",i);}
 nl(){printf("\n");}
 pr(w)ARC w;{
-    I r=AR(w),*d=AD(w),n=AN(w);DO(r,pi(d[i]));nl();
+    I r=AR(w),*d=AD(w),n=AN(w);
+    DO(r,pi(d[i]));
+    nl();
     if(AT(w)==BOX)DO(n,printf("< ");pr(AV(w)[i]))
-    else DO(n,pi(AV(w)[i]));nl();}
+    else DO(n,pi(AV(w)[i]));
+    nl();}
 
 struct{ C c; ARC(*vd)(ARC,ARC); ARC(*vm)(ARC); }ftab[]={
     { 0, 0, 0},
@@ -69,12 +76,12 @@ struct{ C c; ARC(*vd)(ARC,ARC); ARC(*vm)(ARC); }ftab[]={
     { ',', cat, 0 }
 };
 I st[26];
-qp(unsigned a){R  a>='a'&&a<='z';}
+qp(unsigned a){R  islower(a&255);}
 qv(unsigned a){R a<'a';}
 ARC ex(e)I *e;{I a=*e;
  if(qp(a)){if(e[1]=='=')R (ARC)(st[a-'a']=(I)ex(e+2));a= st[ a-'a'];}
  R qv(a)?(ftab[a].vm)(ex(e+1)):e[1]?(ftab[e[1]].vd)((ARC)a,ex(e+2)):(ARC)a;}
-noun(c){ARC z;if(c<'0'||c>'9')R 0;z=ga(0,0,0);*AV(z)=c-'0';R (I)z;}
+noun(c){ARC z;if(!isdigit(c))R 0;z=ga(INT,0,0);*AV(z)=c-'0';R (I)z;}
 verb(c){I i=1;for(;ftab[i].c;)if(ftab[i++].c==c)R i-1;R 0;}
 I *wd(s)C *s;{I a,n=strlen(s),*e=ma(n+1);C c;
  DO(n,e[i]=(a=noun(c=s[i]))?a:(a=verb(c))?a:c);e[n]=0;R e;}
