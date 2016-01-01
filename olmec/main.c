cccf31fc623fad85ce03c50ba32af022cf61c9c2
M Joshua Ryan <luser.droog@gmail.com>
2015-12-31 21:09:18 -0600

    flesh-out main

--- olmec/main.c
+++ olmec/main.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "ed.h"
+
 int main() {
+    char *buf = NULL;
+    char *prompt = "\t";
+    int n;
+
+    if (isatty(fileno(stdin))) specialtty();
+
+    while(get_line(prompt, &buf, &n)){
+        puts(buf);
+    }
+
+    if (isatty(fileno(stdin))) restoretty();
     return 0;
 }
