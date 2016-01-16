0ad44e6c9709c2c8a6c20639a32dac098c004c63
M Joshua Ryan <luser.droog@gmail.com>
2016-01-15 23:15:20 -0600

    export symbols from array module

---
+++ olmec/ar.h
@@ -0,0 +1,38 @@
+#include "../ppnarg.h"
+
+typedef struct ar {
+    int rank;    // number of dimensions
+    int *dims;   // size of each dimension
+    int *weight; // corresponding coefficient in the indexing formula
+    int *data;   // address of first array element
+} *array;
+
+int productdims(int rank, int dims[]);
+array array_new_dims(int rank, int dims[]);
+void loaddimsv(int rank, int dims[], va_list ap);
+array (array_new)(int rank, ...);
+#define array_new(...) (array_new)(PP_NARG(__VA_ARGS__),__VA_ARGS__)
+array cast_dims(int data[], int rank, int dims[]);
+array (cast)(int data[], int rank, ...);
+#define cast(data,...) (cast)(data,PP_NARG(__VA_ARGS__),__VA_ARGS__)
+array clone(array a);
+array copy(array a);
+
+int *elema(array a, int ind[]);
+int *elemv(array a, va_list ap);
+int *elem(array a, ...);
+
+int *vector_index(int ind, int dims[], int n, int vec[]);
+int ravel_index(int vec[], int dims[], int n);
+
+void transpose2(array a);
+void transpose(array a, int shift);
+void transposea(array a, int spec[]);
+array slice(array a, int i);
+array slicea(array a, int spec[]);
+array slices(array a, int s[], int f[]);
+array extend(array a, int extra);
+
+array cat(array x, array y);
+array iota(int n);
+
