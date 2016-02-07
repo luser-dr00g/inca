f9062925d1849fb3c4042233beff11a58863bd07
M Joshua Ryan <luser.droog@gmail.com>
2016-02-06 23:05:18 -0600

    call wd() and examine results.

--- olmec/main.c
+++ olmec/main.c
@@ -1,9 +1,13 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 
+#include "ar.h"
 #include "ed.h"
+#include "en.h"
 #include "io.h"
 #include "st.h"
+#include "wd.h"
 
 symtab env;
 
@@ -21,6 +25,15 @@ int main() {
         //puts(buf);
         for (i=0;i<n;i++)
             printf("%04x ", buf[i]);
+        array a = wd(buf, n);
+        printf("\n");
+        printf("%d\n", a->rank);
+        for (i=0;i<a->rank;i++)
+            printf("%d ", a->dims[i]);
+        printf("\n");
+        for (i=0;i<a->dims[0];i++)
+            printf("%d(%d,%d) ", a->data[i], gettag(a->data[i]), getval(a->data[i]));
+        printf("\n");
     }
 
     if (isatty(fileno(stdin))) restoretty();
