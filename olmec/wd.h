2c73b6949359bcd26f2fa9ea8581615e32b20397
M Joshua Ryan <luser.droog@gmail.com>
2016-02-21 19:25:18 -0600

    add comments

--- olmec/wd.h
+++ olmec/wd.h
@@ -1,4 +1,8 @@
 
+// construct a new object fron n chars starting at s
+// state indicates the type of object to construct
 int newobj(int *s, int n, int state);
+
+// scan up to n chars from s and produce 1D array of encoded expression
 array wd(int *s, int n);
 
