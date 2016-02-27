2c0129e8b060bbce3cda3add60a0ba2f5d45a14d
M Joshua Ryan <luser.droog@gmail.com>
2016-02-27 03:32:10 -0600

    remove newobj from export

--- olmec/wd.h
+++ olmec/wd.h
@@ -1,8 +1,4 @@
 
-// construct a new object fron n chars starting at s
-// state indicates the type of object to construct
-int newobj(int *s, int n, int state);
-
 // scan up to n chars from s and produce 1D array of encoded expression
 array scan_expression(int *s, int n);
 
