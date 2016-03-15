050b80d47e62d32c27eb1644341575a8cf0147fb
M Joshua Ryan <luser.droog@gmail.com>
2016-03-15 01:06:16 -0500

    add semicolon so argument doesn't look funny

--- olmec/debug.h
+++ olmec/debug.h
@@ -1,6 +1,6 @@
 #ifdef DEBUGMODE
     #define DEBUG(LVL,...) if (LVL<=DEBUGMODE) fprintf(stderr, __VA_ARGS__)
-    #define IFDEBUG(LVL,...) do if (LVL<=DEBUGMODE) { __VA_ARGS__ } while(0)
+    #define IFDEBUG(LVL,...) do if (LVL<=DEBUGMODE) { __VA_ARGS__; } while(0)
 #else
     #define DEBUG(...)
     #define IFDEBUG(...)
