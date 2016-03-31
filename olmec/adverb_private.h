f28099b1ac73ec9fc3930b884316604f7c66637a
M Joshua Ryan <luser.droog@gmail.com>
2016-03-31 01:26:33 -0500

    use types

--- olmec/adverb_private.h
+++ olmec/adverb_private.h
@@ -10,10 +10,10 @@ enum { NN, NV, VN, VV };
 
 #define DECLFG \
     verb fv = getptr(v->f); \
-    int (*f1)(int,verb) = fv?fv->monad:0; \
-    int (*f2)(int,int,verb) = fv?fv->dyad:0; \
+    monad *f1 = fv?fv->monad:0; \
+    dyad *f2 = fv?fv->dyad:0; \
     verb gv = getptr(v->g); \
-    int (*g1)(int,verb) = gv?gv->monad:0; \
-    int (*g2)(int,int,verb) = gv?gv->dyad:0; 
+    monad *g1 = gv?gv->monad:0; \
+    dyad *g2 = gv?gv->dyad:0; 
 
 
